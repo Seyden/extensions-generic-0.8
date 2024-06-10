6638a62d49c13271e3af9a47f0893ea739e3edf8
Seyden <saiifii@live.de>
2024-06-10 12:51:26 +0200

    Update version

--- src/MangaStream.ts
+++ src/MangaStream.ts
@@ -43,7 +43,7 @@ import {
 const simpleUrl = require('simple-url')
 
 // Set the version for the base, changing this version will change the versions of all sources
-const BASE_VERSION = '3.0.4'
+const BASE_VERSION = '3.0.5'
 export const getExportVersion = (EXTENSION_VERSION: string): string => {
     return BASE_VERSION.split('.')
                        .map((x, index) => Number(x) + Number(EXTENSION_VERSION.split('.')[index]))
